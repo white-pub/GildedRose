dbfe829f2d03b8608df997f90c897dbf6c563324
Lin2025-888 <s96024n@gmail.com>
2025-03-20 23:59:12 -0500

    fix failed foo_test and finish updated update_quality logic

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,41 +1,67 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
+    NORMAL_QUALITY_DECREASE = 1
+
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
+
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+            
+            # items that just decrease by value everyday, no special rules
+            is_normal_item = True
+
+            # Sulfuras is never changed
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+
+            # decrease/increase quality
+            if item.name == "Aged Brie" and item.quality < 50:
+                is_normal_item = False
+                item.quality = item.quality + 1
+            
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                is_normal_item = False
+                if item.sell_in < 6:
+                    item.quality = item.quality + 3
+                elif item.sell_in < 11:
+                    item.quality = item.quality + 2
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = item.quality + 1
+                # can't have quality > 50
+                if item.quality > 50:
+                    item.quality = 50
+            
+            if item.name == "Conjured Mana Cake":
+                is_normal_item = False
+                item.quality = item.quality - 2 * self.NORMAL_QUALITY_DECREASE
+            
+            if is_normal_item:
+                item.quality = item.quality - self.NORMAL_QUALITY_DECREASE
+            
+            # all item sell_in decrease
+            item.sell_in = item.sell_in - 1
+
+            # handle items that have negative sell_in
+            if item.sell_in < 0:
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = item.quality - item.quality
+                
+                if item.name == "Conjured Mana Cake":
+                    item.quality = item.quality - self.NORMAL_QUALITY_DECREASE
+
+                if is_normal_item:
+                    item.quality = item.quality - self.NORMAL_QUALITY_DECREASE
+            
+            # quality is never negative
+            if item.quality < 0:
+                item.quality = 0
 
 
+# Item class should not be changed
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
